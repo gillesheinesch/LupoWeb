b7ae6bd2d78bd2500ec5442bd492fe853d8f7769
Nickkel <mail.nickkel@gmail.com>
2021-07-28 14:42:12 +0200

    Setup project

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Button } from 'rsuite';
+
+export default class TopNavbar extends Component {
+
+    render() {
+        return (
+          <Button appearance="primary"><i class="fab fa-discord"></i> Login</Button>
+        )
+    }
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'rsuite';
+
+function HomePage() {
+
+  console.log(process.env.REST_API_URL);
+  return (
+    <div/>
+  )
+}
+
+export default HomePage;
